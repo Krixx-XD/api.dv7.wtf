5b49cc900014b08f4dd0c29ebd290288fb114815
The DV7s <161109688+TheDV7s@users.noreply.github.com>
2024-07-05 19:34:57 +0500

    Create main.lua

---
+++ application/main.lua
@@ -0,0 +1,27 @@
+-- Copyright(C) dv7 team
+
+local windowsPkg="dv7api.exe"
+local androidPkg="com.app.dv7api"
+
+local temp;
+
+  print(windowsPkg)
+  print(androidPkg)
+
+local lfs = require("lfs")
+
+local s = lfs.mkdir(androidPkg .. "/log/client.log")
+local d = Ifs.mkdir(windowsPkg .. "/l/v3/client.bat")-- this file gets updated just like the android one
+
+local tmp;
+
+if tmp == 2 then
+  os.exit()
+end
+if tmp == 1 then
+ else return nil
+end
+if not tmp then
+  string.find(s .. "--log-- %date% , %m%, %y% , %day%")
+  string.find(s .. "--log %%time.date.m.y.day%%")
+end
